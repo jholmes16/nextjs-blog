74f266a95f2023925f08f69529588e218130baf7
jholmes16 <jholmesarea@gmail.com>
2021-08-12 10:32:02 -0400

    add post components

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,47 @@
 import { Fragment } from "react";
+import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React framwork for production -it makes building fullstack React apps a breeze.",
+    date: "2021-08-10",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React framwork for production -it makes building fullstack React apps a breeze.",
+    date: "2021-08-10",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React framwork for production -it makes building fullstack React apps a breeze.",
+    date: "2021-08-10",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React framwork for production -it makes building fullstack React apps a breeze.",
+    date: "2021-08-10",
+  },
+];
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
